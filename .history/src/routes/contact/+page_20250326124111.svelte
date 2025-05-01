c3764798e6d05101f3729b443ce2c6aad5b01cab
hallo <strong.victory.2002@gmail.com>
2025-05-01 15:33:52 -0400

    contacts

--- .history/src/routes/contact/+page_20250326124111.svelte
+++
@@ -1,9 +0,0 @@
-<div class="bg-gray-700">
-    <div class="mx-12 flex flex-col items-center bg-slate-400 h-fit">
-        <div class="my-12">
-            <div class="text-center">
-                <p class="text-4xl">About The Company</p>
-            </div>
-        </div>
-    </div>
-</div>
--- .history/src/routes/contact/+page_20250326124140.svelte
+++
@@ -1,9 +0,0 @@
-<div class="bg-gray-700">
-    <div class="mx-12 flex flex-col items-center bg-slate-400 h-fit">
-        <div class="my-12">
-            <div class="text-center">
-                <p class="text-4xl">Contacts</p>
-            </div>
-        </div>
-    </div>
-</div>
