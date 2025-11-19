6d15c3381f675dedc91d71a2ad12e0e59fef036a
hallo <strong.victory.2002@gmail.com>
2025-11-18 21:30:16 -0500

    update umami

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,11 +15,7 @@
 </script>
 
 <svelte:head>
-    <script
-        defer
-        src="http://gwf-umami.67.207.84.31.sslip.io/script.js"
-        data-website-id="b356c86c-09c1-4dcd-9f32-febd48023d6e"
-    ></script>
+    <script defer src="http://limboworks-umami.67.207.84.31.sslip.io/script.js" data-website-id="b356c86c-09c1-4dcd-9f32-febd48023d6e"></script>
 </svelte:head>
 <div class="drawer drawer-end text-neutral-50">
     <input id="my-drawer-3" type="checkbox" class="drawer-toggle" bind:checked={checked}/>
